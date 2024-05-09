4f072dda7e4204c10e034ff87bb3b650cff08e12
sbot50 <xyz>
2024-05-09 16:28:02 +0200

    I dont get it

--- Commands/ping.js
+++ Commands/ping.js
@@ -11,6 +11,8 @@ module.exports = {
       .setColor(0x1cd0ce)
       .setDescription('🏓 pong\n' + client.ws.ping + ' ms')
     console.log("test");
+    console.log(interaction);
+    console.log(interaction.editReply)
     await interaction.editReply({ content: " ", embeds: [embed] });
     console.log("should have replied!");
   }
